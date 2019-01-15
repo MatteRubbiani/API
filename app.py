4a32c7d64e622e86a32059b6ffbb57a509b305ec
Matteo Rubbiani <Matteo@iMac-di-Maria.home>
2019-01-15 18:37:02 +0100

    zxc

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ from resources.daytimetable import OrarioGiorno
 from resources.put import Put
 
 app= Flask(__name__)
-#app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URL","sqlite:///data.db")
-app.config["SQLALCHEMY_TRAK_MODIFICATIONS"]=False
+app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("HEROKU_POSTGRESQL_COBALT_URL","sqlite:///data.db")
+#app.config["SQLALCHEMY_TRAK_MODIFICATIONS"]=False
 app.secret_key="Matteo"
 api=Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///data.db"
