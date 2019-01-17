0ee2bc3d8474fb08fe9139cc9fef8adf21269e2f
Matteo Rubbiani <Matteo@iMac-di-Maria.home>
2019-01-17 16:50:32 +0100

    dddd

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ jwt = JWT (app, authenticate, identity)
 app.config['JWT_AUTH_USERNAME_KEY'] = 'mail'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(hours=5)
 
-@app.before_first_request
-def create_table():
-    db.create_all()
+#@app.before_first_request
+#def create_table():
+    #db.create_all()
 
 
 
