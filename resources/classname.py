10985c31193dd47cddbca08cd2ab87e911fc81b4
Matteo Rubbiani <Matteo@iMac-di-Maria.home>
2019-02-10 21:06:55 +0100

    sss

--- resources/classname.py
+++ resources/classname.py
@@ -1,16 +1,16 @@
 from flask_jwt import jwt_required
 from models.users import UserModel
-from models.classes import ClassModel, find_by_tag
+from models.classes import ClassModel, find_by_id
 from flask_restful import Resource, request
 class ClassName(Resource):
 
 
     #@jwt_required()
-    def post(self):
+    def get(self):
         mail=request.args.get('mail')
         user=UserModel.find_by_mail(mail)
         if user:
-            classe=ClassModel.find_by_id(user.classe_id)
+            classe=find_by_id(user.classe_id)
             if classe:
                 return classe.nome
             return {"message":"user has no class"}, 200
