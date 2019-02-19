07348d3906dbf0abcd2420e89cfccef33a101784
Matteo Rubbiani <Matteo@iMac-di-Maria.home>
2019-02-19 21:01:03 +0100

    f

--- resources/authenticate.py
+++ resources/authenticate.py
@@ -8,9 +8,8 @@ from models.users import UserModel
 import hashlib, uuid
 class UserLogin(Resource):
     def post(self):
-        data=request.get_json()
-        mail=data["mail"]
-        password=data["password"]
+        mail=request.args.get('mail')
+        password=request.args.get('password')
         user=UserModel.find_by_mail(mail)
         epsw=password.encode('utf-8')
         if user: #and user.password==hashlib.sha512(epsw).hexdigest() and user.confirmed==True:
@@ -26,6 +25,6 @@ class TokenRefresh(Resource):
     def post(self):
         current_user=get_jwt_identity()
         #a=get_raw_jwt()
-        #return a["iat"] questo E' PER PENDERE DATA DI CRAZIONE REFRESH TOKEN 
+        #return a["iat"] questo E' PER PENDERE DATA DI CRAZIONE REFRESH TOKEN
         new_token=create_access_token(identity=current_user, fresh=False)
         return {"access_token":new_token}
