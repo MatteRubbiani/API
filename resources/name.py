b538090984a4e4eac58939dcb370cd9fe61166ee
Matteo Rubbiani <Matteo@iMac-di-Maria.home>
2019-02-22 21:31:49 +0100

    q

--- resources/name.py
+++ resources/name.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 class Name(Resource):
+    @jwt_required
     def get (self):
         current_user=get_jwt_identity()
         user=UserModel.find_by_id(current_user)
